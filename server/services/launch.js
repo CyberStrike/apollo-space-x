758c4707b415b51e7f5b7578817cb98fc62968f1
Chris Scott <cyberstrike@gmail.com>
2021-08-25 01:57:44 -0400

    ✨ ADD: Lauch Service - get a launch or launches by id

--- server/services/launch.js
+++ server/services/launch.js
@@ -7,6 +7,43 @@ class LaunchAPI extends RESTDataSource {
     super()
     this.baseURL = baseURL
   }
+
+  launchReducer (launch) {
+    return {
+      id: launch.flight_number || 0,
+      cursor: `${launch.launch_date_unix}`,
+      site: launch.launch_site && launch.launch_site.site_name,
+      mission: {
+        name: launch.mission_name,
+        missionPatchSmall: launch.links.mission_patch_small,
+        missionPatchLarge: launch.links.mission_patch
+      },
+      rocket: {
+        id: launch.rocket.rocket_id,
+        name: launch.rocket.rocket_name,
+        type: launch.rocket.rocket_type
+      }
+    }
+  }
+
+  async getAllLaunches () {
+    const response = await this.get('launches')
+    return Array.isArray(response)
+      ? response.map((launch) => this.launchReducer(launch))
+      : []
+  }
+
+  async getLaunchById ({ launchId }) {
+    const endpoint = 'launches'
+    const response = await this.get(endpoint, { flight_number: launchId })
+    return Array.isArray(response) ? this.launchReducer(response[0]) : []
+  }
+
+  getLaunchesById ({ launchIds }) {
+    return Promise.all(
+      launchIds.map((launchId) => this.getLaunchById({ launchId }))
+    )
+  }
 }
 
 module.exports = LaunchAPI
