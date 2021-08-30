b9e3857b315bad051632a7107b71320b76f450c1
Chris Scott <cyberstrike@gmail.com>
2021-08-30 04:30:14 -0400

    🛠 CFG: Add Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,13 @@ module.exports = {
     node: true,
     commonjs: true,
     es2021: true,
-    es6: true
+    es6: true,
+    'jest/globals': true
   },
-  extends: ['standard'],
+  extends: ['standard', 'plugin:jest/recommended', 'plugin:jest/style'],
   parserOptions: {
     ecmaVersion: 12
   },
+  plugins: ['jest'],
   rules: {}
 }
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  verbose: true,
+  testPathIgnorePatterns: ['/node_modules/', '/__utils']
+}
+
+module.exports = config
